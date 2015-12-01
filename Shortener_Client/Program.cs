420c037957a64d4b45dfa97009844392cd5f4a55
Craig Miller <craigm1357@gmail.com>
2015-12-01 00:55:50 +0000

    Added Get(id), Post, Delete, & Put method functionality.

--- Shortener_Client/Program.cs
+++ Shortener_Client/Program.cs
@@ -41,8 +41,9 @@ namespace Shortener_Client
             Console.WriteLine("7.  GET blob for existing sample");
 
             Console.WriteLine("0.  Exit the application.");
-
+            Console.WriteLine();
             Console.WriteLine("\nSelect an option by entering a number..");
+            Console.WriteLine();
 
             int input = 0;
             bool fine = int.TryParse(Console.ReadLine(), out input);
@@ -50,6 +51,7 @@ namespace Shortener_Client
             if (fine)
             {
                 bool valid;
+                Console.WriteLine();
                 switch (input)
                 {
                     case 0:
@@ -63,6 +65,7 @@ namespace Shortener_Client
 
                         //GET all samples
                         Console.WriteLine("Getting data on all samples.");
+                        Console.WriteLine();
                         await GetAll();
 
                         break;
@@ -70,12 +73,12 @@ namespace Shortener_Client
                     case 2:
 
                         Console.WriteLine("Enter the ID of the Sample you want info on");
-
+                        Console.WriteLine();
                         input = 0;
                         valid = int.TryParse(Console.ReadLine(), out input);
                         if (valid)
                         {
-                            GetOne(input);
+                            await GetOne(input);
                         } else
                         {
                             await ShowMenu();
@@ -86,18 +89,21 @@ namespace Shortener_Client
 
                     case 3:
                         Console.WriteLine("Posting hard coded data.");
-                        Post();
+                        Console.WriteLine();
+                        await Post();
 
                         break;
 
                     case 4:
 
                         Console.WriteLine("Enter the ID of the Sample you want to delete");
+                        Console.WriteLine();
+
                         input = 0;
                         valid = int.TryParse(Console.ReadLine(), out input);
                         if (valid)
                         {
-                            Delete(input);
+                           await Delete(input);
                         }
                         else
                         {
@@ -108,12 +114,13 @@ namespace Shortener_Client
 
                     case 5:
 
-                        Console.WriteLine("Enter the ID of the sample you want override");
+                        Console.WriteLine("Enter the ID of the sample you want overwrite");
+                        Console.WriteLine();
                         input = 0;
                         valid = int.TryParse(Console.ReadLine(), out input);
                         if (valid)
                         {
-                            Put(input);
+                           await Put(input);
                         }
                         else
                         {
@@ -125,6 +132,8 @@ namespace Shortener_Client
                     case 6:
 
                         Console.WriteLine("Enter the ID of the blob you want to put to");
+                        Console.WriteLine();
+
                         input = 0;
                         valid = int.TryParse(Console.ReadLine(), out input);
                         if (valid)
@@ -141,6 +150,8 @@ namespace Shortener_Client
                     case 7:
 
                         Console.WriteLine("Enter the ID of the blob you want to get");
+                        Console.WriteLine();
+
                         input = 0;
                         valid = int.TryParse(Console.ReadLine(), out input);
                         if (valid)
@@ -157,54 +168,105 @@ namespace Shortener_Client
                     default:
 
                         Console.WriteLine("Invalid input integer, try again..");
+                        Console.WriteLine();
                         await ShowMenu();
                         break;
                 } 
             } else
             {
                 Console.WriteLine("Please enter an integer");
+                Console.WriteLine();
+
                 await ShowMenu();
             }
 
         }
 
-
-
         private static async Task GetAll()
         {
             //Get all samples
             HttpResponseMessage response;
-            response = await client.GetAsync("api/Samples");
+            response = await client.GetAsync("api/samples");
             if (response.IsSuccessStatusCode)
             {
                 IEnumerable<Sample> samples = await response.Content.ReadAsAsync<IEnumerable<Sample>>();
                 Console.WriteLine("Samples:");
+                Console.WriteLine();
+
+                Console.WriteLine("{0}\t{1}\t{2}\t{3}", "ID     ", "Title     ", "Artist     ", "Date     ");
+                Console.WriteLine("{0}\t{1}\t{2}\t{3}", "--     ", "-----     ", "------     ", "----     ");
                 foreach (var sample in samples)
                 {
-                    Console.WriteLine("{0}\t{1}\t${2}\t{3}", sample.SampleID, sample.Title, sample.Artist, sample.DateOfSampleCreation);
+                    Console.WriteLine("{0}     \t{1}     \t{2}     \t{3}", sample.SampleID, sample.Title, sample.Artist, sample.DateOfSampleCreation);
                 }
             }
             await ReturnToMenu();
         }
 
-        private static void GetOne(int id)
+        private static async Task GetOne(int id)
         {
             //Get a sample by id
+            HttpResponseMessage response;
+            response = await client.GetAsync("api/samples/" + id);
+            if (response.IsSuccessStatusCode)
+            {
+
+                Sample sample = await response.Content.ReadAsAsync<Sample>();
+                Console.WriteLine("Samples:");
+
+                Console.WriteLine("{0}\t{1}\t${2}\t{3}", sample.SampleID, sample.Title, sample.Artist, sample.DateOfSampleCreation);
+
+            }
+            await ReturnToMenu();
         }
 
-        private static void Post()
+        private static async Task Post()
         {
             //Post a new sample
+            Random rand = new Random();
+            var sample = new Sample() { Title = "Craigs song v" + rand.Next(50), Artist = "Craig", DateOfSampleCreation = DateTime.Now };
+            HttpResponseMessage response = await client.PostAsJsonAsync("api/samples", sample);
+            if (response.IsSuccessStatusCode)
+            {
+                Console.WriteLine("Sucessfully Posted new Sample to the api");
+            }
+
+            await ReturnToMenu();
         }
 
-        private static void Delete(int id)
+        private static async Task Delete(int id)
         {
             //Delete a sample
+            HttpResponseMessage response;
+            response = await client.DeleteAsync("api/samples/" + id);
+
+            if (response.IsSuccessStatusCode)
+            {
+                Console.WriteLine("Deleted sample at id: " + id);
+            }
+
+            await ReturnToMenu();
         }
 
-        private static void Put(int id)
+        private static async Task Put(int id)
         {
-            //Get information to an existing record
+            //Overwrite information in an existing record by ID
+            HttpResponseMessage response;
+            response = await client.GetAsync("api/samples/" + id);
+            if (response.IsSuccessStatusCode)
+            {
+                Sample sample = await response.Content.ReadAsAsync<Sample>();
+
+                //Update record by prepending 'New'to its Title
+                sample.Title = "New " + sample.Title;
+                response = await client.PutAsJsonAsync("api/samples/" + id, sample);
+                if (response.IsSuccessStatusCode)
+                {
+                    Console.WriteLine("Successfully updated Sample at ID" + id + " via PUT");
+                }
+            }
+
+            await ReturnToMenu();
         }
 
         private static void PutBlob(int id)
@@ -219,6 +281,7 @@ namespace Shortener_Client
 
         private static async Task ReturnToMenu()
         {
+            Console.WriteLine();
             Console.WriteLine("Press any key to continue");
             Console.ReadKey();
             await ShowMenu();
